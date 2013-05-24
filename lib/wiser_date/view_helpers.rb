41af2cb9fba84a5a714bf9867130bb1f4d309cdf
Kenneth John Balgos <kennethjohnbalgos>
2013-05-24 18:33:38 +0800

    Add time_first and hide_same_year

--- lib/wiser_date/view_helpers.rb
+++ lib/wiser_date/view_helpers.rb
@@ -13,6 +13,9 @@ module WiserDate
       date_format = options.has_key?(:date_format) ? options[:date_format] : "%b %d, %Y"
       time_format = options.has_key?(:time_format) ? options[:time_format] : "%l:%M%P"
       humanize = options.has_key?(:humanize) ? options[:humanize] : true
+      time_first = options.has_key?(:time_first) ? options[:time_first] : false
+      hide_same_year = options.has_key?(:hide_same_year) ? options[:hide_same_year] : true
+      
     
       # Now
       time_now = Time.now
@@ -20,7 +23,7 @@ module WiserDate
       # Formats
       flat_format = "%Y%m%d%H%M%S"
       plain_format = "%Y-%m-%d %H:%M:%S"
-      custom_format = "#{time_format} #{date_format}"
+      custom_format = time_first ? "#{time_format} #{date_format}" : "#{date_format} #{time_format}"
     
       # Timestamps
       custom_timestamp = timestamp.strftime(custom_format)
@@ -32,10 +35,12 @@ module WiserDate
         if time_now.strftime(plain_format) == timestamp.strftime(plain_format)
           custom_timestamp = "just now"
         elsif time_now.to_date - 1.day == timestamp.to_date 
-          custom_timestamp = "#{timestamp.strftime(time_format)} yesterday"
+          date_value = "yesterday"
+          time_value = timestamp.strftime(time_format)
+          custom_timestamp = time_first ? "#{time_value} #{date_value}" : "#{date_value} #{time_value}"
         elsif time_diff < 1.0
           custom_timestamp = "#{distance_of_time_in_words(Time.now, timestamp.to_time)} ago"
-        elsif time_now.year == timestamp.year 
+        elsif hide_same_year && time_now.year == timestamp.year 
           custom_timestamp = custom_timestamp.gsub(", #{time_now.year.to_s}", '')
         end
       end
