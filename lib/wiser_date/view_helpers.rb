954810570343d5ed41072f320694fabb9584241d
Kenneth John Balgos <kennethjohnbalgos>
2013-05-25 05:00:35 +0800

    Allow custom_class and time_now options. Handle over time. Adjust Just Now.

--- lib/wiser_date/view_helpers.rb
+++ lib/wiser_date/view_helpers.rb
@@ -2,12 +2,12 @@ require 'date'
 
 module WiserDate
   module ViewHelpers
-    
+  
     include ActionView::Helpers::JavaScriptHelper
     include ActionView::Helpers::TagHelper
     include ActionView::Helpers::DateHelper
     include ActionView::Context    
-  
+
     def wiser_date(timestamp, options = {})
       # Options
       date_format = options.has_key?(:date_format) ? options[:date_format] : "%b %d, %Y"
@@ -16,51 +16,62 @@ module WiserDate
       time_first = options.has_key?(:time_first) ? options[:time_first] : false
       hide_same_year = options.has_key?(:hide_same_year) ? options[:hide_same_year] : false
       capitalize = options.has_key?(:capitalize) ? options[:capitalize] : true
-    
-  
-      # Now
-      time_now = Time.now
-  
+      custom_class = options.has_key?(:custom_class) ? options[:custom_class] : nil
+      time_now = options.has_key?(:time_now) ? options[:time_now].to_datetime : Time.now
+
       # Formats
       flat_format = "%Y%m%d%H%M%S"
       plain_format = "%Y-%m-%d %H:%M:%S"
       custom_format = time_first ? "#{time_format} #{date_format}" : "#{date_format} #{time_format}"
-  
+
       # Timestamps
       custom_timestamp = timestamp.strftime(custom_format)
       plain_timestamp = timestamp.strftime(plain_format)
-  
+
       # Humanize Display
       if humanize
         time_diff_in_seconds = (time_now - timestamp.to_time).ceil
-        time_diff = (time_diff_in_seconds / (60*60*24))
-        if time_diff_in_seconds <= 15
+        time_diff_in_days = (time_diff_in_seconds / (60*60*24))
+        if time_diff_in_seconds <= 30 && time_diff_in_seconds >= 0
           custom_timestamp = "just now"
         elsif time_now.to_date - 1.day == timestamp.to_date 
           date_value = "yesterday"
           time_value = timestamp.strftime(time_format)
           custom_timestamp = time_first ? "#{time_value} #{date_value}" : "#{date_value} #{time_value}"
-        elsif time_diff < 1.0
+        elsif time_diff_in_days <= 1.0 && time_diff_in_days >= 0
           custom_timestamp = "#{distance_of_time_in_words(Time.now, timestamp.to_time)} ago"
         elsif hide_same_year && time_now.year == timestamp.year 
           custom_timestamp = custom_timestamp.gsub(", #{time_now.year.to_s}", '')
         end
       end
-    
+  
       # Capitalize
       if capitalize
         custom_timestamp = custom_timestamp.to_s.capitalize
       end
-  
+
+    
       uniq_id = Digest::SHA1.hexdigest([Time.now, rand].join)
       html = content_tag(:span, custom_timestamp, 
         "id" => uniq_id,
-        "class" => "wiser_date", 
+        "class" => "wiser_date #{custom_class if custom_class.present?}", 
         "data-plain-timestamp" => plain_timestamp, 
         "data-custom-timestamp" => custom_timestamp,
         "data-new-custom-timestamp" => custom_timestamp
       )
-      html += javascript_tag("jQuery(document).ready(function(){if(jQuery('body meta#wiser_date').size() == 0){$('body').prepend('<meta id=\"wiser_date\" data-value=\"#"+uniq_id+"\"/>')}else{$('meta#wiser_date').attr('data-value',$('meta#wiser_date').attr('data-value')+', #"+uniq_id+"')}});")
+    
+      meta_vars = []
+      meta_vars << "data-value=\"#"+uniq_id+"\""
+      meta_vars << "data-custom-format=\""+custom_format+"\""
+      meta_vars << "data-date-format=\""+date_format+"\""
+      meta_vars << "data-time-format=\""+time_format+"\""
+      meta_vars << "data-humanize=\""+humanize.to_s+"\""
+      meta_vars << "data-hide-same-year=\""+hide_same_year.to_s+"\""
+      meta_vars << "data-time-first=\""+time_first.to_s+"\""
+      meta_vars << "data-capitalize=\""+capitalize.to_s+"\""
+      meta_vars << "id=\"wiser_date\""
+    
+      html += javascript_tag("jQuery(document).ready(function(){if(jQuery('body meta#wiser_date').size() == 0){$('body').prepend('<meta "+meta_vars.join(' ')+" />')}else{$('meta#wiser_date').attr('data-value',$('meta#wiser_date').attr('data-value')+', #"+uniq_id+"')}});")
       html.html_safe
     end
   end
--- wiser_date.gemspec
+++ wiser_date.gemspec
@@ -1,13 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'wiser_date'
-  s.version     = '0.1.2'
-  s.date        = '2012-05-24'
+  s.version     = '0.1.3'
+  s.date        = '2012-05-25'
   s.summary     = "Display dates in the coolest format."
   s.description = "Display dates in the coolest format."
   s.authors     = ["Kenneth John Balgos"]
   s.email       = 'kennethjohnbalgos@gmail.com'
-  s.files       = ["lib/wiser_date/view_helpers.rb", "lib/wiser_date/railtie.rb", "lib/wiser_date.rb"]
-  s.homepage    = 'http://rubygems.org/gems/wiser_date'
+  s.files       = ["lib/wiser_date/view_helpers.rb", "lib/wiser_date/railtie.rb", "lib/wiser_date.rb", "README.rdoc"]
   s.license     = 'MIT'
   s.homepage    = "https://github.com/kennethjohnbalgos/wiser_date"
 end
