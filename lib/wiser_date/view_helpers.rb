920e00f11b3424950dc6cb3f0095abe7f18b51d3
Kenneth John Balgos <kennethjohnbalgos>
2013-05-24 16:30:38 +0800

    Build 0.0.7

--- lib/wiser_date/view_helpers.rb
+++ lib/wiser_date/view_helpers.rb
@@ -4,17 +4,25 @@ module WiserDate
   module ViewHelpers
     include ActionView::Helpers::JavaScriptHelper
     include ActionView::Helpers::TagHelper
+    include ActionView::Helpers::DateHelper
     include ActionView::Context    
       
     def wiser_date(timestamp, options = {})
-      if options.has_key?(:format)
-        format = options[:format] 
-      else
-        format = "%l:%M%p %b %d, %Y"
+      # Options
+      format = options.has_key?(:format) ? options[:format] : "%l:%M%p %b %d, %Y"
+      humanize = options.has_key?(:humanize) ? options[:humanize] : true
+      
+      # Timestamps
+      custom_timestamp = timestamp.strftime(format)
+      plain_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+      
+      # Humanize Display
+      if humanize && ((Time.now - timestamp.to_time).to_f / (60*60*24)).to_i >= 1
+        custom_timestamp = distance_of_time_in_words(Time.now, timestamp.to_time)
       end
-      new_timestamp = timestamp.strftime(format)
-      html = content_tag(:div, new_timestamp, :class => "wiser_date", "data-timestamp" => new_timestamp)
-      html += javascript_tag("jQuery(document).ready(function(){ alert('nice'); });")
+      
+      html = content_tag(:div, custom_timestamp, :class => "wiser_date", "data-timestamp" => plain_timestamp)
+      html += javascript_tag("jQuery(document).ready(function(){});")
       html.html_safe
     end
     
