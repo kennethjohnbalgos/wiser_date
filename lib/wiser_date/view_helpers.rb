5a1cea9718cb58c560ffa80492f6e4737c8f737d
Kenneth John Balgos <kennethjohnbalgos>
2013-05-24 16:10:58 +0800

    Build 0.0.6

--- lib/wiser_date/view_helpers.rb
+++ lib/wiser_date/view_helpers.rb
@@ -2,7 +2,6 @@ require 'date'
 
 module WiserDate
   module ViewHelpers
-    FORMAT_REPLACEMENTES = { "yy" => "%Y", "mm" => "%m", "dd" => "%d", "d" => "%-d", "m" => "%-m", "y" => "%y", "M" => "%b"}
     include ActionView::Helpers::JavaScriptHelper
     include ActionView::Helpers::TagHelper
     include ActionView::Context    
@@ -13,19 +12,10 @@ module WiserDate
       else
         format = "%l:%M%p %b %d, %Y"
       end
-      new_timestamp = format_date(timestamp, format)
+      new_timestamp = timestamp.strftime(format)
       html = content_tag(:div, new_timestamp, :class => "wiser_date", "data-timestamp" => new_timestamp)
       html += javascript_tag("jQuery(document).ready(function(){ alert('nice'); });")
       html.html_safe
-    end
-    
-    def format_date(tb_formatted, format)
-      new_format = translate_format(format)
-      Date.parse(tb_formatted).strftime(new_format)
-    end
-    
-    def translate_format(format)
-      format.gsub!(/#{FORMAT_REPLACEMENTES.keys.join("|")}/) { |match| FORMAT_REPLACEMENTES[match] }
     end
     
   end
