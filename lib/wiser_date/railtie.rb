eab32b691eed6b5e497c1930f00ccc4dec7c3c2e
Kenneth John Balgos <kennethjohnbalgos>
2013-05-26 15:37:45 +0800

    Build 0.1.6

--- lib/wiser_date/railtie.rb
+++ lib/wiser_date/railtie.rb
@@ -1,6 +1,8 @@
 require "wiser_date/view_helpers"
-
 module WiserDate
+  class Engine < ::Rails::Engine
+  end
+  
   class Railtie < Rails::Railtie
     initializer "wiser_date.view_helpers" do |app|
       ActionView::Base.send :include, ViewHelpers
